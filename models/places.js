a59c4ff69d63c324f50bc94b02747ec93eff85c2
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2022-11-19 13:27:50 +0000

    Adding Bootstrap grid to index page

--- models/places.js
+++ models/places.js
@@ -1,15 +1,15 @@
 let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: 'images/thai-pan-asian.jpg'
+}, {
+  name: 'Coding Cat Cafe',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery',
+  pic: 'images/coding-cat-cafe.jpg'
 }];
 
 module.exports = places;
--- views/home.jsx
+++ views/home.jsx
@@ -3,11 +3,20 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
+        <Def>
+            <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/chia-fruit-drink.jpg" 
+                        alt="Chia Fruit Shake"
+                        width='500' />
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                    </div>
+                </div>
+
                 <a href="/places" className='btn btn-primary'>Places page</a>
-          </main>
+            </main>
       </Def>
     )
 }  
