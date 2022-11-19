c1466d54c8821c4328c22d95673b2ad732098efb
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2022-11-19 13:37:09 +0000

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -2,14 +2,22 @@ const express = require("express")
 const router = express.Router()
 const places = require('../models/places');
 
+// INDEX
 router.get('/', (req, res) => {
     res.render('places/index', {
         places
     });
 });
 
+// NEW
 router.get('/new', (req, res) => {
     res.render('places/new')
-})  
+});
+
+// CREATE
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+});
 
 module.exports = router
