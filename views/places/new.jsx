0afda968e322e64205b734c7ee907741549b1aa0
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2022-11-24 16:55:19 +0000

    added alidations

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -10,36 +10,58 @@ function new_form () {
             <form method="POST" action="/places" className='row p-3'>
                 <div className='form-group col-sm-6'>
                     <label htmlFor="name">Place Name</label>
-                    <input type="text" id="name" name="name" required className='form-control' />
+                    <input type="text" 
+                        id="name" 
+                        name="name" 
+                        required 
+                        className='form-control' />
                 </div>
 
                 <div className='form-group col-sm-6'>
                     <label htmlFor="pic">Place Picture</label>
-                    <input type="url" id="pic" name="pic" className='form-control' />
+                    <input type="url" 
+                        id="pic" 
+                        name="pic" 
+                        className='form-control' />
                 </div>
 
                 <div className='form-group col-sm-6'>
                     <label htmlFor="city">City</label>
-                    <input type="text" id="city" name="city" className='form-control' />
+                    <input type="text" 
+                        id="city" 
+                        name="city" 
+                        className='form-control' />
                 </div>
 
                 <div className='form-group col-sm-6'>
                     <label htmlFor="state">State</label>
-                    <input type="text" id="state" name="state" className='form-control' />
+                    <input type="text" 
+                        id="state" 
+                        name="state" 
+                        className='form-control' />
                 </div>
 
                 <div className='form-group col-sm-6'>
                     <label htmlFor="cuisines">Cuisines</label>
-                    <input type="text" id="cuisines" name="cuisines" required className='form-control' />
+                    <input type="text" 
+                        id="cuisines" 
+                        name="cuisines" 
+                        required 
+                        className='form-control' />
                 </div>
 
                 <div className="form-group col-sm-6">
-                    <label for="founded">Founded Year</label>
-                    <input className="form-control" id="founded" name="founded" />
+                    <label htmlFor="founded">Founded Year</label>
+                    <input type="number" 
+                        id="founded" 
+                        name="founded" 
+                        className="form-control"
+                        // min="1673"
+                        // max={new Date().getFullYear()}
+                        value={new Date().getFullYear()}/>
                 </div>
 
                 <div className='form-group'>
-                    <br />
                     <button type="submit" className='btn btn-primary'>Add Place</button>
                 </div>
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -10,14 +10,16 @@ function show (data) {
                         <img src={`${data.place.pic}`} 
                             alt={data.place.name} 
                             className="img-fluid" />
+                        <h3>Located in {data.place.city + ', ' + data.place.state}</h3>
                     </div>
 
                     <div className="col">
                         <h1>{data.place.name}</h1>
                         <h2>Rating</h2>
-                        <p>No rating</p>
+                        <p>Not rated</p>
                         <h2>Description</h2>
-                        <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+                        <h3>{data.place.showEstablished()}</h3>
+                        <h4>Serving {data.place.cuisines}</h4>
 
                         <div className='d-flex justify-content-center'>
                             <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
